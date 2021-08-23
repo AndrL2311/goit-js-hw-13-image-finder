73d7a066750f53943cd7c5db10b9f2ed8d61eb19
Andrii Lypovetskyi <li_andr@ukr.net>
2021-08-23 09:58:40 +0300

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -8,18 +8,33 @@ export default class ApiService{
     this.searchQuery = '';
     this.page = 1;
    }
-  
-  fetchGallery() {
+  // Функция реализована через промисы
+  // fetchGallery() {
+  // // console.log(this);
+  // const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+  // //  console.log(url);
+  // return fetch(url)
+  //   .then(response => response.json())
+  //   .then(({ hits }) => {
+  //     this.incrementPage();
+  //     return hits;
+  //   })
+  //   .catch(error => console.log(error));
+  // }
+
+  // Функция реализована через асинхронные функции
+  async fetchGallery() {
   // console.log(this);
   const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
   //  console.log(url);
-  return fetch(url)
-    .then(response => response.json())
-    .then(({ hits }) => {
+    const response = await fetch(url);
+    const hits = await response.json();
+    const item = ({ hits }) => {
       this.incrementPage();
       return hits;
-    })
-    .catch(error => console.log(error));
+    };
+    // console.log(hits);
+    return await item(hits);
   }
   
     incrementPage() {
